56260c1cdf0016f82af468898ed2f635cfe03401
duy1711ak <77217315+duy1711ak@users.noreply.github.com>
2022-03-16 20:53:17 +0700

    add magnetic switch

--- main.ts
+++ main.ts
@@ -1,8 +1,18 @@
+let door_status = 0
 basic.forever(function () {
     NPNBitKit.DHT11Read(DigitalPin.P0)
     basic.pause(500)
     serial.writeString("!7:TEMP:" + NPNBitKit.DHT11Temp() + "#")
-    basic.pause(1000)
+    basic.pause(500)
     serial.writeString("!7:HUMID:" + NPNBitKit.DHT11Hum() + "#")
-    basic.pause(2000)
+    basic.pause(500)
+    serial.writeString("!1:DOOR:" + pins.digitalReadPin(DigitalPin.P1) + "#")
+    door_status = pins.digitalReadPin(DigitalPin.P1)
+    basic.pause(500)
+    if (door_status == 0) {
+        basic.showIcon(IconNames.Yes)
+    } else {
+        basic.showIcon(IconNames.No)
+    }
+    basic.pause(500)
 })
