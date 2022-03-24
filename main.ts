98ad7ceb542c887b1ddf7f12cdd794882db3e6fb
PiQuiHac <94162067+PiQuiHac@users.noreply.github.com>
2022-03-24 15:56:59 +0700

    gan full ne

--- main.ts
+++ main.ts
@@ -6,7 +6,6 @@ function dht11 () {
     NPNLCD.ShowNumber(temp, 5, 0)
     NPNLCD.ShowString("Humi:", 8, 0)
     NPNLCD.ShowNumber(humi, 13, 0)
-    basic.pause(100)
     serial.writeString("!7:TEMP:" + temp + "#")
     basic.pause(100)
     serial.writeString("!7:HUMID:" + humi + "#")
@@ -15,30 +14,27 @@ function dht11 () {
 function light_sensor () {
     light_level = pins.analogReadPin(AnalogPin.P4)
     if (light_level <= 300) {
-        pins.digitalWritePin(DigitalPin.P5, 1)
-        pins.digitalWritePin(DigitalPin.P6, 1)
+        pins.digitalWritePin(DigitalPin.P3, 1)
+        pins.digitalWritePin(DigitalPin.P4, 1)
     } else {
-        pins.digitalWritePin(DigitalPin.P5, 0)
-        pins.digitalWritePin(DigitalPin.P6, 0)
+        pins.digitalWritePin(DigitalPin.P3, 0)
+        pins.digitalWritePin(DigitalPin.P4, 0)
     }
 }
 function door () {
     if (NPNBitKit.ButtonDoorOpen(DigitalPin.P1)) {
-        pins.digitalWritePin(DigitalPin.P3, 1)
-        pins.digitalWritePin(DigitalPin.P4, 0)
+        pins.digitalWritePin(DigitalPin.P5, 1)
+        pins.digitalWritePin(DigitalPin.P6, 0)
         serial.writeString("!8:MAGNETIC:" + "1" + "#")
     } else {
-        pins.digitalWritePin(DigitalPin.P3, 0)
-        pins.digitalWritePin(DigitalPin.P4, 1)
+        pins.digitalWritePin(DigitalPin.P5, 0)
+        pins.digitalWritePin(DigitalPin.P6, 1)
         serial.writeString("!8:MAGNETIC:" + "0" + "#")
     }
-    basic.pause(100)
 }
 function gas () {
     gas_raw = pins.analogReadPin(AnalogPin.P10)
     gas_percent = Math.map(gas_raw, 0, 1023, 0, 100)
-    NPNLCD.ShowString("Gas:", 0, 1)
-    NPNLCD.ShowNumber(gas_percent, 4, 1)
     if (gas_percent >= 54) {
         pins.digitalWritePin(DigitalPin.P2, 1)
         serial.writeString("!23:GAS:" + "1" + "#")
@@ -54,9 +50,9 @@ let temp = 0
 led.enable(false)
 NPNLCD.LcdInit()
 basic.forever(function () {
-    light_sensor()
-    door()
     gas()
     dht11()
+    door()
+    light_sensor()
     basic.pause(1000)
 })
